bf39866442c60864660f3c16c573c7024a69def8
Destin Frank <159183449+DestinFrank@users.noreply.github.com>
2024-04-04 13:53:49 -0400

    Double down

---
+++ 1.2-Variables.js
@@ -0,0 +1,9 @@
+let length = 10
+let width = 5
+let area = (length * width)
+console.log('Area: ' + area);
+console.log('\n')
+let first_name = 'Santa '
+let last_name = 'Claws'
+last_name = 'Claus'
+console.log('I still believe in ' + first_name + last_name);
