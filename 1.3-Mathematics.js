542245e3875a55a602c10672111a96d9fb834f21
Destin Frank <159183449+DestinFrank@users.noreply.github.com>
2024-04-04 14:10:05 -0400

    i Didded it

---
+++ 1.3-Mathematics.js
@@ -0,0 +1,12 @@
+let celsius = 100;
+let farenheit = 0;
+celsius = celsius + 2;
+farenheit = (celsius * 9/5) + 32;
+console.log('100 degrees celsius is ' + farenheit + ' degrees farenheit!');
+console.log("\n");
+farenheit = 77;
+celsius = (farenheit - 32) * 5/9;
+console.log('77 degrees farenheit is ' + celsius + ' degrees celsius!');
+console.log('\n')
+let m = ((0 - 5)/(6 - (-4)));
+console.log(m);
